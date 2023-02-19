3ee2015380dd027a5f68d47108475f0028a588d0
moppywhip <bkzhao38631@gmail.com>
2023-02-18 16:21:17 -0800

    updated schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -5,25 +5,35 @@ export default defineSchema({
     messages: defineTable({
         body: s.string(),
         user: s.id("users"),
-        convoID: s.string()
+        convoID: s.string(),
+        time:s.bigint(),
+        msgId:s.bigint()
       }),
 
     users: defineTable({
     name: s.string(),
-    tokenIdentifier: s.string(),
+    userid: s.bigint(),
     email: s.string(),
     description:s.string(),
     phone: s.string()
-    }).index("by_token", ["tokenIdentifier"]),
+    }).index("by_token", ["userid"]),
 
     posts: defineTable({
-
+        postId:s.bigint(),
         title:s.string(),
         date:s.string(),
         description:s.string(),
         author:s.string(),
-        tags:s.any()
+        tags:s.any(),
 
     }),
+
+    requests: defineTable({
+
+        from:s.string(),
+        to:s.string(),
+        requestId:s.bigint(),
+
+    })
 
 });
